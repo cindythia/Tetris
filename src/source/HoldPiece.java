bb7cad7ce28025c6f7a6bac2aec5824949b2af0a
WinstonT-27 <61402561+WinstonT-27@users.noreply.github.com>
2020-03-19 11:02:12 +0700

    Update HoldPiece.java

--- src/source/HoldPiece.java
+++ src/source/HoldPiece.java
@@ -9,20 +9,50 @@ public class HoldPiece extends Piece {
 		super(block, coords, board, color);
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public bool coord_point(int x, int y)
+	{
+		if(coords[y][x]!=0)	{
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	public int len(int n)
+	{
+		if (coords[n].length !=4 && coords[n].length !=2){
+			return 0;
+		}
+		else if (coords[n].length ==4){
+			return 1;
+		return -1;
+	}
 	@Override
 	public void render(Graphics g) {
 		for (int y = 0; y < coords.length; y++) {
 			for (int x = 0; x < coords[y].length; x++) {
-				if (coords[y][x] != 0 && coords[0].length != 4 && coords[0].length != 2)
+				//if (coords[y][x] != 0 && coords[0].length != 4 && coords[0].length != 2)
+				//	g.drawImage(block, x * Board.BLOCKSIZE + 425, y * Board.BLOCKSIZE + 2 * board.getIndentY() + 80,
+				//			null);
+				//else if (coords[y][x] != 0 && coords[0].length == 4)
+				//	g.drawImage(block, x * Board.BLOCKSIZE + 415, y * Board.BLOCKSIZE + 2 * board.getIndentY() + 85,
+				//			null);
+				//else if (coords[y][x] != 0)
+				//	g.drawImage(block, x * Board.BLOCKSIZE + 440, y * Board.BLOCKSIZE + 2 * board.getIndentY() + 80,
+				//			null);
+				
+				if(coord_point(x, y) ==true && len(0)==0){
 					g.drawImage(block, x * Board.BLOCKSIZE + 425, y * Board.BLOCKSIZE + 2 * board.getIndentY() + 80,
 							null);
-				else if (coords[y][x] != 0 && coords[0].length == 4)
+				else if(coords_point(x, y)==true && len(0)==1){
 					g.drawImage(block, x * Board.BLOCKSIZE + 415, y * Board.BLOCKSIZE + 2 * board.getIndentY() + 85,
 							null);
-				else if (coords[y][x] != 0)
+				}
+				else if (coords_point(x, y)==true){
 					g.drawImage(block, x * Board.BLOCKSIZE + 440, y * Board.BLOCKSIZE + 2 * board.getIndentY() + 80,
 							null);
+				}
 			}
 		}
 	}
